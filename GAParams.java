8107d3492daae29a96bb613f9e4179ef7041506b
Piotr Barnuś <barney5@o2.pl>
2013-06-16 18:43:09 +0200

    Suchy nawet-nie-szkielet, nie dziala jeszcze nic

---
+++ GAParams.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class GAParams implements Serializable{
+	int subjectlist[];
+	int teacherlist[][];
+	int popsize;
+	int ngen;
+	double pmut;
+	double pcross;
+	double best_fitness[];
+	int teacher_ids[][];
+	int room_ids[][];
+	int class_ids[][];
+	int subject_ids[][];
+}
